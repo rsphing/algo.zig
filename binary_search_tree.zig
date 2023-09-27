e9f68675ef86f677bdf8b404d295e707273a610a
ShihPQ <46019875@qq.com>
2023-09-27 11:52:20 +0800

    learning

--- binary_search_tree.zig
+++ binary_search_tree.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
 const TreeNode = @import("binary_tree.zig").TreeNode;
 
 pub fn BinarySearchTree(comptime T: type) type {
@@ -7,17 +8,39 @@ pub fn BinarySearchTree(comptime T: type) type {
         const Self = @This();
 
         root: ?*TreeNode(T) = null,
+        arena: ArenaAllocator = undefined,
         gpa: Allocator = undefined,
 
         pub fn init(gpa: Allocator, root: ?*TreeNode(T)) Self {
+            var arena = ArenaAllocator.init(gpa);
             return .{
-                .gpa = gpa,
+                .arena = arena,
+                .gpa = arena.allocator(),
                 .root = root,
             };
         }
 
         pub fn deinit(self: *Self) void {
-            _ = self;
+            self.arena.deinit();
+        }
+
+        pub fn fromSlice(self: *Self, slice: []T) !void {
+            const build = (struct {
+                fn _build(gpa: Allocator, sl: []T) !?*TreeNode(T) {
+                    if (sl.len == 0) return null;
+
+                    var mid = sl.len / 2;
+                    var node = try gpa.create(TreeNode(T));
+                    node = .{ .data = sl[mid] };
+
+                    if (mid >= 1) node.left = try _build(gpa, sl[0..mid]);
+                    node.right = try _build(gpa, sl[mid + 1 ..]);
+                    return node;
+                }
+            })._build;
+
+            std.mem.sort(T, slice, {}, comptime std.sort.asc(T));
+            self.root = try build(self.gpa, slice);
         }
 
         pub fn search(self: *Self, val: T) ?*TreeNode(T) {
@@ -44,11 +67,11 @@ pub fn BinarySearchTree(comptime T: type) type {
             }
 
             var cur = self.root;
-            var pre: *TreeNode(T) = undefined;
+            var pre: ?*TreeNode(T) = null;
             while (cur != null) {
                 if (cur.?.data == val) return;
 
-                pre = cur.?;
+                pre = cur;
 
                 if (cur.?.data > val) {
                     cur = cur.?.left;
@@ -59,16 +82,35 @@ pub fn BinarySearchTree(comptime T: type) type {
 
             var tmp = try self.gpa.create(TreeNode(T));
             tmp = .{ .data = val };
-            if (pre.data > val) {
-                pre.left = tmp;
+            if (pre.?.data > val) {
+                pre.?.left = tmp;
             } else {
-                pre.right = tmp;
+                pre.?.right = tmp;
             }
         }
 
         pub fn remove(self: *Self, val: T) !void {
             if (self.root == null) return;
-            _ = val;
+
+            var cur = self.root;
+            var pre: ?*TreeNode(T) = null;
+            while (cur != null) {
+                if (cur.?.data == val) break;
+
+                pre = cur;
+
+                if (cur.?.data > val) {
+                    cur = cur.?.left;
+                } else {
+                    cur = cur.?.right;
+                }
+            }
+
+            if (cur == null) return;
+            if (cur.?.left == null or cur.?.right == null) {
+                var node = if (cur.?.left != null) cur.?.left else cur.?.right;
+                if (node != null) {}
+            }
         }
     };
 }
